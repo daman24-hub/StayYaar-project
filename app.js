fa3eb48e258551c5099728e31892de4e630809e5
daman24-hub <daman.24bai10507@vitbhopal.ac.in>
2026-06-06 02:34:50 +0530

    fix cookie expires to use Date object

--- app.js
+++ app.js
@@ -73,10 +73,10 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    },
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), 
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+},
 };
 app.use(session(sessionOptions));
 app.use(flash());
